4ccb277f27e681835490d62ed1a22508f59036f7
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-06-02 19:47:26 -0700

    login mutation  in resolvers successfully tested in sandbox, returns user info and token id

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,31 @@
-const {User} = require('../models')
-const {signToken} = require('../utils/auth')
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
- Mutation: {
-     addUser: async (parent, { username, email, password }) => {
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
- }
-}
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
