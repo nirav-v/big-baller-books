ac592c86e066ff762f9330baaafa38ab666a8907
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-06-02 19:37:52 -0700

    addUser resolver fixed so that the query returns an Auth type w token ID and instance of user model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,12 @@
 const {User} = require('../models')
+const {signToken} = require('../utils/auth')
 
 const resolvers = {
  Mutation: {
      addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
-    //   const token = signToken(user);
-      return { username };
+      const token = signToken(user);
+      return { token, user };
     },
  }
 }
